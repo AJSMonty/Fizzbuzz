20b303cdc00fba11319e77952cbfd14644cdedb5
AJ <aj.monty@gmail.com>
2020-11-12 15:56:23 +0000

    defined fizzbuzz within Integer Class

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,33 +3,37 @@ require 'fizzbuzz'
 describe 'fizzbuzz' do
 
     it 'returns fizz when given 3' do
-        expect(fizzbuzz(3)).to eq 'fizz'
+        expect(3.fizzbuzz).to eq 'fizz'
     end
 
     it 'returns fizz when given 12' do
-        expect(fizzbuzz(12)).to eq 'fizz'
+        expect(12.fizzbuzz).to eq 'fizz'
     end
 
     it 'returns buzz when given 5' do
-        expect(fizzbuzz(5)).to eq 'buzz'
+        expect(5.fizzbuzz).to eq 'buzz'
     end
 
     it 'returns buzz when given 20' do
-        expect(fizzbuzz(20)).to eq 'buzz'
+        expect(20.fizzbuzz).to eq 'buzz'
     end
 
     it 'returns fizzbuzz when given 15' do
-        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+        expect(15.fizzbuzz).to eq 'fizzbuzz'
     end
 
     it 'returns number if not multiple of 5 or 3' do
-        expect(fizzbuzz(7)).to eq 7
+        expect(7.fizzbuzz).to eq 7
     end
 
-    examples = { 1 => 1, 2 => 2}
+    it 'returns "fizz" for the number 3' do
+        expect(3.fizzbuzz).to eq 'fizz'
+    end
+
+    examples = {1 => 1, 2 => 2, 3 => 'fizz', 5 => 'buzz', 50 =>'buzz', 150 =>'fizzbuzz'}
     examples.each do |number, result|
       it "returns #{result} when passed #{number}" do
-      expect(fizzbuzz(number)).to eq result
+      expect(number.fizzbuzz).to eq result
     end
   end
 end
