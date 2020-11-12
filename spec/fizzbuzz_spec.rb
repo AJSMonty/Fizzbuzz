5bf223c1ced9a9a7f538a15b55b19f05efe61815
Chris Whitehouse <chris.whitehouse@hotmail.com>
2020-11-12 15:07:01 +0000

    solved 5 returns buzz spec, added 12 returns fizz spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,7 +6,14 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
 
+    it 'returns fizz when given 12' do
+        expect(fizzbuzz(12)).to eq 'fizz'
+    end
+
+
     it 'returns buzz when given 5' do
         expect(fizzbuzz(5)).to eq 'buzz'
     end
+
+
 end
