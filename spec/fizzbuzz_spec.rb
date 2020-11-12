8f2e8c0c88b6d6d5dde34996d702b12fa54c05fe
AJ <aj.monty@gmail.com>
2020-11-12 15:02:08 +0000

    added new spec for buzz'

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,12 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+
     it 'returns fizz when given 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
+    end
+
+    it 'returns buzz when given 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
     end
 end
